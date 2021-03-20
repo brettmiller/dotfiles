63e5d951c58aa353dde9a1d69b210eefc1ea2b41
Brett Miller <github@shadowed.net>
2021-03-20 16:18:41 -0500

    Update setup.sh
    reset HEAD --hard to keep from erring on existing files

--- .dotfiles/setup.sh
+++ .dotfiles/setup.sh
@@ -2,6 +2,8 @@ git clone --bare https://github.com/brettmiller/dotfiles.git $HOME/.dotfiles
 
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
+# reset HEAD --hard so we don't get errors about existing files
+dotfiles reset HEAD --hard
 dotfiles checkout
 dotfiles submodule update --init
 # after --init submodules will be detatched from HEAD, checkout default branch and pull
