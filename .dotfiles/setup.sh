db6b0d9868e4bb673eebd90f4bed2b991c83bcb2
Brett Miller <github@shadowed.net>
2021-03-20 15:42:23 -0500

    Update setup.sh
    checkout default branch of submodules so they are detached from HEAD

--- .dotfiles/setup.sh
+++ .dotfiles/setup.sh
@@ -4,3 +4,5 @@ alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 dotfiles checkout
 dotfiles submodule update --init
+# after --init submodules will be detatched from HEAD, checkout default branch and pull
+dotfiles submodule foreach 'default_branch="$(git symbolic-ref refs/remotes/origin/HEAD)"; git checkout ${default_branch##*/} && git pull'
