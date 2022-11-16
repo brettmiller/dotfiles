dee17f301412f388ecc37169eb46fac471a6fe6e
Brett Miller <github@shadowed.net>
2022-11-16 09:23:12 -0600

    additional bash gcloud completion path

--- .bash.d/gcloud.bash
+++ .bash.d/gcloud.bash
@@ -3,7 +3,7 @@ if [[ -r /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.
   source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc"
 fi
 
-gcp_completion=("/usr/lib64/google-cloud-sdk/completion.bash.inc" "/usr/share/google-cloud-sdk/completion.bash.inc" "/usr/local/share/google/google-cloud-sdk/completion.bash.inc" "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc")
+gcp_completion=("/usr/lib64/google-cloud-sdk/completion.bash.inc" "/usr/share/google-cloud-sdk/completion.bash.inc" "/usr/local/share/google/google-cloud-sdk/completion.bash.inc" "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc" "/snap/google-cloud-sdk/current/completion.bash.inc")
 
 for file in ${gcp_completion[@]}; do
   [[ -r $file ]] && source $file
