89b6118d9f1f9034cc58f58081f4cbecb34ba989
Brett Miller <github@shadowed.net>
2022-12-11 02:37:29 -0600

    check for kubectl before running completion script

--- .bash_completion.d/kubectl.sh
+++ .bash_completion.d/kubectl.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-TMPKCB=/tmp/KCB${RANDOM}
-
-kubectl completion bash > $TMPKCB
-
-. $TMPKCB
-rm $TMPKCB
+if command -v kubectl; then
+  TMPKCB=/tmp/KCB${RANDOM}
+  
+  kubectl completion bash > $TMPKCB
+  
+  . $TMPKCB
+  rm $TMPKCB
+fi
