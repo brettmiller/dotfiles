44ce7f1c4f25afa4c4edec5abd695701f63c16f0
Brett Miller <github@shadowed.net>
2022-12-11 02:26:36 -0600

    completion-ruby.sh actually test for file

--- .bash_completion.d/completion-ruby.sh
+++ .bash_completion.d/completion-ruby.sh
@@ -2,6 +2,6 @@
 #
 # cloned from https://github.com/mernen/completion-ruby/
 # 
-if [[ ${HOME}/.bash_completion.d/completion-ruby/completion-ruby-all ]]; then
+if [[ -f ${HOME}/.bash_completion.d/completion-ruby/completion-ruby-all ]]; then
   source ${HOME}/.bash_completion.d/completion-ruby/completion-ruby-all
 fi
